214843f70a128690aa43dc7c88fd2504de15ac49
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-08-10 20:00:13 +0430

    create Card item class

--- 001-AddToCard_Items/Add_To_Card.py
+++ 001-AddToCard_Items/Add_To_Card.py
@@ -8,5 +8,13 @@ class Product:
     def reduce_stock(self , quantity):
         if quantity <= self.stock:
             self.stock -= quantity
+        else:
+            print(f"Not enough stock for {self.name}")
     def increase_stock(self ,quantity):
         self.stock += quantity
+
+# Define Cart Item class
+class CardItem:
+    def __init__(self , product , quantity):
+        self.product = product
+        self.quantity = quantity
